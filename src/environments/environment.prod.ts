52b32e7d8d12e094819495d2e4d5dbc865f81cd1
patricsteiner <patric.steiner1@gmail.com>
2020-05-08 16:56:58 +0200

    add angularfire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyDEUEYdA3-0_SbwPFmxEFaTjT-aUdpkfHo',
+    authDomain: 'retro-nyx.firebaseapp.com',
+    databaseURL: 'https://retro-nyx.firebaseio.com',
+    projectId: 'retro-nyx',
+    storageBucket: 'retro-nyx.appspot.com',
+    messagingSenderId: '1051120116381',
+    appId: '1:1051120116381:web:90ce03900e415e8c736a74',
+    measurementId: 'G-2E8YDLJJ50',
+  },
 };
