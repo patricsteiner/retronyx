24bc1b81120f4d379b46363f1c706a01a8a213d6
Patric Steiner <patric.steiner1@gmail.com>
2020-05-14 17:29:16 +0200

    ng lint

--- src/app/board/color-hash.pipe.ts
+++ src/app/board/color-hash.pipe.ts
@@ -4,6 +4,24 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'colorHash',
 })
 export class ColorHashPipe implements PipeTransform {
+  private static hashCode(str: string): number {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      // tslint:disable-next-line:no-bitwise
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
+  private static intToHex(i: number): string {
+    // tslint:disable-next-line:no-bitwise
+    const c = (i & 0x00ffffff).toString(16).toUpperCase();
+    return '00000'.substring(0, 6 - c.length) + c;
+  }
+
+  private static contrast(hexcolor) {
+    return parseInt(hexcolor, 16) > 0xffffff / 2 ? '000000' : 'ffffff';
+  }
   transform(value: string, contrast?: boolean): string {
     if (!value) {
       return contrast ? '#EEEEEE' : '#AAAAAA';
@@ -13,22 +31,5 @@ export class ColorHashPipe implements PipeTransform {
       hex = ColorHashPipe.contrast(hex);
     }
     return '#' + hex;
-  }
-
-  private static hashCode(str: string): number {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    return hash;
-  }
-
-  private static intToHex(i: number): string {
-    let c = (i & 0x00ffffff).toString(16).toUpperCase();
-    return '00000'.substring(0, 6 - c.length) + c;
-  }
-
-  private static contrast(hexcolor) {
-    return parseInt(hexcolor, 16) > 0xffffff / 2 ? '000000' : 'ffffff';
   }
 }
